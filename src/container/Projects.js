91f54dcd6dec6313761ce76db9466702eb16b0d6
Nyore <aamonthedon@gmail.com>
2023-03-17 16:09:22 +0100

    changed project structure, styled scrollbar

---
+++ src/container/Projects.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Container } from "./styles/Container.styled";
+import { SectionTitle } from "./styles/About.styled";
+import { StyledWork } from "./styles/Work.styled";
+import Work from "./Work";
+
+const Projects = () => {
+  return (
+    <StyledWork id="work">
+      <Container>
+        <SectionTitle style={{ color: "var(--bluish-purple)", top: "3rem" }}>
+          My Projects
+        </SectionTitle>
+        <Work />
+      </Container>
+    </StyledWork>
+  );
+};
+
+export default Projects;
